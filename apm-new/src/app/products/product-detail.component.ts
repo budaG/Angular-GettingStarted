d9471c34a2f18ac82fdb705f5a571558aee04564
gbudhathoki <gbudhathoki@gaig.com>
2022-10-22 11:24:52 -0400

    added new project

---
+++ apm-new/src/app/products/product-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Prouct Details';
+  product: IProduct | undefined;
+  imageWidth: number = 150;
+
+  constructor(private route: ActivatedRoute,
+            private router: Router,
+            private productService: ProductService) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+    this.product =   {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2021",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    };
+
+  }
+
+  onBack(): void{
+    this.router.navigate(['/products']);
+  }
+
+}
