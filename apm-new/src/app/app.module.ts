6c5b781603b53a5b8fd745258577517d2b309e99
ganesh <ganesh.budhathoki88@gmail.com>
2022-10-24 12:23:43 -0400

    Angular getting started completed

--- apm-new/src/app/app.module.ts
+++ apm-new/src/app/app.module.ts
@@ -1,42 +1,25 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './shared/page-not-found.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
-    WelcomeComponent,
-    PageNotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {path: 'product/:id', 
-        canActivate:[ProductDetailGuard],
-        component: ProductDetailComponent},
-      {path: 'welcome', component: WelcomeComponent},
-      {path: '', redirectTo:'welcome', pathMatch:'full'},
-      {path: '**', component:PageNotFoundComponent}
-    ])
-  ],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		RouterModule.forRoot([
+			{ path: 'welcome', component: WelcomeComponent },
+			{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
+			{ path: '**', component: PageNotFoundComponent },
+		]),
+		ProductModule,
+	],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- apm-new/src/app/products/product-list.component.ts
+++ apm-new/src/app/products/product-list.component.ts
@@ -1,64 +1,62 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { IProduct } from "./product";
-import { ProductService } from "./product.service";
-import {Subscription} from "rxjs";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    templateUrl:'./product-list.component.html',
-    styleUrls:['./product-list.component.css']
+	templateUrl: './product-list.component.html',
+	styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit, OnDestroy{
-    pageTitle: string = 'Product List';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
-    errorMessage: string = '';
-    sub!: Subscription;
-    private _listFilter: string = '';
+export class ProductListComponent implements OnInit, OnDestroy {
+	pageTitle: string = 'Product List';
+	imageWidth: number = 50;
+	imageMargin: number = 2;
+	showImage: boolean = false;
+	errorMessage: string = '';
+	sub!: Subscription;
+	private _listFilter: string = '';
 
-    get listFilter(): string {
-      return this._listFilter;
-    }
+	get listFilter(): string {
+		return this._listFilter;
+	}
 
-    set listFilter(value:string) {
-      this._listFilter = value;
-      this.filteredProducts = this.performFilter(value);
-    }
+	set listFilter(value: string) {
+		this._listFilter = value;
+		this.filteredProducts = this.performFilter(value);
+	}
 
-    filteredProducts: IProduct[] = [];
+	filteredProducts: IProduct[] = [];
 
-    products: IProduct[] = [];
+	products: IProduct[] = [];
 
-    constructor(private productService: ProductService) {
-    }
+	constructor(private productService: ProductService) {}
 
-    performFilter(filterBy: string): IProduct[] {
-      filterBy = filterBy.toLocaleLowerCase();
-      return this.products.filter((product: IProduct) =>
-        product.productName.toLocaleLowerCase().includes(filterBy)
-      );
-    }
+	performFilter(filterBy: string): IProduct[] {
+		filterBy = filterBy.toLocaleLowerCase();
+		return this.products.filter((product: IProduct) =>
+			product.productName.toLocaleLowerCase().includes(filterBy)
+		);
+	}
 
-    toggleImage() : void {
-        this.showImage = !this.showImage;
-        
-    }
+	toggleImage(): void {
+		this.showImage = !this.showImage;
+	}
 
-    ngOnInit(): void {
-      this.productService.getProduct().subscribe({
-        next: products => {
-          this.products = products;
-          this.filteredProducts = this.products;
-        },
-        error: err => this.errorMessage = err
-      })
-    }
+	ngOnInit(): void {
+		this.productService.getProduct().subscribe({
+			next: (products) => {
+				this.products = products;
+				this.filteredProducts = this.products;
+			},
+			error: (err) => (this.errorMessage = err),
+		});
+	}
 
-    ngOnDestroy(): void {
-      // this.sub.unsubscribe();
-    }
+	ngOnDestroy(): void {
+		// this.sub$.unsubscribe();
+	}
 
-    onRatingClicked(message: string): void{
-      this.pageTitle = 'Product List: ' + message;
-    }
+	onRatingClicked(message: string): void {
+		this.pageTitle = 'Product List: ' + message;
+	}
 }
